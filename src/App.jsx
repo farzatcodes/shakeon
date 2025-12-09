706f735eb9fa57a148a4d1ba9c9b0ee18879ac96
Farzat Amin <68422219+farzatcodes@users.noreply.github.com>
2025-12-10 04:46:20 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ const App = () => {
           <div className="flex items-center gap-2 cursor-pointer" onClick={() => scrollToSection('hero')}>
             {/* LOGO PLACEHOLDER */}
             <img 
-              src="../images/logo.png" 
+              src="/logo.png" 
               alt="Shakeon Logo" 
               className="h-10 w-auto object-contain rounded shadow-sm"
             />
@@ -141,7 +141,7 @@ const App = () => {
             
             {/* HERO PRODUCT PLACEHOLDER */}
             <img 
-              src="../images/hero.png" 
+              src="/hero.png" 
               alt="Shakeon Hero Bottle" 
               className="relative z-10 h-full w-auto object-contain drop-shadow-2xl transform group-hover:-translate-y-2 transition-transform duration-500"
             />
@@ -224,7 +224,7 @@ const App = () => {
               
               {/* PRODUCT IMAGE 1 PLACEHOLDER */}
               <img 
-                src="../images/banana.png" 
+                src="/banana.png" 
                 alt="Banana Cinnamon Bottle" 
                 className="absolute bottom-0 left-1/2 transform -translate-x-1/2 h-72 w-auto object-contain drop-shadow-xl"
               />
@@ -246,7 +246,7 @@ const App = () => {
               
                {/* PRODUCT IMAGE 2 PLACEHOLDER */}
                <img 
-                src="../images/dates.png" 
+                src="/dates.png" 
                 alt="Dates Almond Bottle" 
                 className="absolute bottom-0 left-1/2 transform -translate-x-1/2 h-80 w-auto object-contain drop-shadow-xl"
               />
@@ -267,7 +267,7 @@ const App = () => {
               
               {/* PRODUCT IMAGE 3 PLACEHOLDER */}
               <img 
-                src="../images/chocolate.png" 
+                src="/chocolate.png" 
                 alt="Chocolate Chickpea Bottle" 
                 className="absolute bottom-0 left-1/2 transform -translate-x-1/2 h-72 w-auto object-contain drop-shadow-xl"
               />
